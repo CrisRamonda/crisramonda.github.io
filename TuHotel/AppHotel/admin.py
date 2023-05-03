cd75c58d5e4a0b8cfe1e7ef12d0adf2bc9e0d861
CrisRamonda <71059098+CrisRamonda@users.noreply.github.com>
2023-05-03 09:08:54 -0300

    Delete TuHotel directory

--- TuHotel/AppHotel/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Cliente)
-admin.site.register(Habitacion)
-admin.site.register(Reserva)
-admin.site.register(Avatar)
